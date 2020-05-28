19307bf48f175b058fdce0bad77f04d09ce91f6f
Buttonupd <Dankariuki0101@gmail.com>
2020-05-28 14:02:20 +0300

    Uploaded cerain modules/packages inside the development environment

--- mistergram/models.py
+++ mistergram/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+from friendship.models import Friend, Follow, Block
 
-# Create your models here.
+
